c54d621f1ced37685fb409eadf5ba2a231956ace
Abhishek Thakur <th.abhishek12@gmail.com>
2022-02-01 17:28:00 +0530

    disable button when input is empty

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,8 +1,12 @@
-import React, {useRef} from "react";
+import React, {useRef, useState} from "react";
 import { UseAppStore } from "../store";
 
 
 export const Input = () =>{
+    const [inputPerson, setInput] = useState("");
+    const handleInChange = (e: React.ChangeEvent<HTMLInputElement>)=>{
+        setInput(e.target.value);
+    } 
     const inputRef = useRef<HTMLInputElement>(null);
     const addPerson = UseAppStore(state=> state.addPerson)
     const add = () =>{
@@ -13,8 +17,8 @@ export const Input = () =>{
     }
     return(
         <div>
-            <input className="inp" type="text" ref={inputRef} placeholder="persone name" />
-                <button disabled = {!inputRef.current?.value ? false : true} onClick={add} className="btn btn-in">Add Person</button>
+            <input className="inp" type="text" name="inputPerson" onChange={handleInChange} ref={inputRef} placeholder="person name" />
+                <button disabled = {!inputPerson} onClick={add} className="btn btn-in">Add Person</button>
                 {/* <button disabled = {inputRef.current?.value ===""} onClick={add}>Add Person</button> */}
         </div>
     )
